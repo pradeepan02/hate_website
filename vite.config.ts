a05829e6cc8392e37273878ecbc2a9d8b96d8550
pradeep <pradeep02012005@gmail.com>
2025-10-15 23:47:03 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,17 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["hate-website.onrender.com"], // ✅ Add this line
+    allowedHosts: ["hate-website.onrender.com"],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins:[react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-    chunkSizeWarningLimit: 1000,
   },
 }));
